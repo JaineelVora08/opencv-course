da5f06f414a59e9733e5a31885f909f90776765b
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-22 15:01:16 +0530

    Update rescale_resize.py

--- Section #2 - Advanced/rescale_resize.py
+++ Section #2 - Advanced/rescale_resize.py
@@ -16,7 +16,7 @@ def rescaleFrame(frame, scale=0.75):
 
 def changeRes(width,height):
     # Live video
-    capture.set(3,width)
+    capture.set(3,width)    # doubt-only works for live videos - so photos/video files ko is method se resize nai kar sakte
     capture.set(4,height)
     
 # Reading Videos
@@ -35,3 +35,10 @@ while True:
 
 capture.release()
 cv.destroyAllWindows()
+
+# Now, we usually resize and rescale video files and images to prevent computational strain
+enerally, it's always best practice to downscale or
+change the width and height of your video files to a smaller value than the original
+dimensions. The reason for this is because while most cameras your webcam included, do
+not support going higher than its maximum capability. So for example, if a camera shoots
+in 720 P, chances are it's not going to be able to shoot in 1080 P or higher. So to rescale
